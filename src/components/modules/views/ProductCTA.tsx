39e88255a842ae1a8a0a54b9ad3a921d56add79c
Ryan Long <ryan@saltycatfish.com>
2021-05-04 00:23:05 -0400

    Testing form submission x4 with netlify-form mod

--- src/components/modules/views/ProductCTA.tsx
+++ src/components/modules/views/ProductCTA.tsx
@@ -7,7 +7,7 @@ import Typography from "../components/Typography";
 import TextField from "../components/TextField";
 import Snackbar from "../components/Snackbar";
 import Button from "../components/Button";
-import { NetlifyForm, Honeypot } from "react-netlify-forms";
+
 import cta from "../../img/cta.png";
 
 const styles = (theme: Theme) =>
@@ -78,15 +78,14 @@ function ProductCTA(props: WithStyles<typeof styles>) {
       <Grid container>
         <Grid item xs={12} md={6} className={classes.cardWrapper}>
           <div className={classes.card}>
-            <NetlifyForm
+            <form
               name="keep_updated_form"
-              action="/thanks"
-              honeypotName="bot-field"
               id="keep_updated_form"
+              data-netlify="true"
               onSubmit={handleSubmit}
               className={classes.cardContent}
             >
-              <Honeypot />
+              <input type="hidden" name="form-name" value="keep_updated_form" />
               <Typography variant="h2" component="h2" gutterBottom>
                 Let's Drive
               </Typography>
@@ -94,6 +93,8 @@ function ProductCTA(props: WithStyles<typeof styles>) {
                 We'll update you via email with rides and events.
               </Typography>
               <TextField
+                label="email"
+                name="email"
                 noBorder
                 className={classes.textField}
                 placeholder="Your email"
@@ -107,7 +108,7 @@ function ProductCTA(props: WithStyles<typeof styles>) {
               >
                 Keep me updated
               </Button>
-            </NetlifyForm>
+            </form>
           </div>
         </Grid>
         <Grid item xs={12} md={6} className={classes.imagesWrapper}>
